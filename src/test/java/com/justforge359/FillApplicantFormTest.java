9682fafc77b2223a2b8b595dc1f2cdbc2f95da39
Mikhail Ustinov <mikhailu359@gmail.com>
2023-06-21 13:35:21 +0300

    changes kommit

--- src/test/java/com/justforge359/FillApplicantFormTest.java
+++ src/test/java/com/justforge359/FillApplicantFormTest.java
@@ -1,51 +1,45 @@
 package com.justforge359;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-public class HomeworkTest {
+import static com.codeborne.selenide.Selenide.*;
+
+public class FillApplicantFormTest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com/";
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+    }
 
 
     @Test
     void successfulTest() {
-        Configuration.browserSize = "1920x1080";
-        //открыть страницу https://demoqa.com/automation-practice-form
-        open("https://demoqa.com/automation-practice-form");
-        //заполнить форму FirstName
+        open("automation-practice-form");
         $("#firstName").setValue("Mikhail");
-        //заполнить форму LastName
         $("#lastName").setValue("Ustinov");
-        //заполнить форму Email
         $("#userEmail").setValue("dsvsdv@mail.com");
-        //заполнить форму Gender
         $("#genterWrapper").$(byText("Male")).click();
-        //заполнить форму Mobile(10 Digits)
         $("#userNumber").setValue("1234567890");
-        //заполнить форму Date of Birth
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-dropdown-container").$(byText("May")).click();
         $(".react-datepicker__year-dropdown-container").$(byText("1986")).click();
         $(".react-datepicker__month").$(byText("10")).click();
-        //заполнить форму Subjects
         $("#subjectsInput").setValue("Maths").pressEnter();
-        //заполнить форму Hobbies
         $("#hobbiesWrapper").$(byText("Reading")).click();
-        //заполнить форму Picture
         $("#uploadPicture").uploadFromClasspath("picture.png");
-        //заполнить форму Current Address
         $("#currentAddress").setValue("fbv;oksdfolibvjsdifb");
-        //заполнить форму State and City
         $("#state").click();
         $("#state").$(byText("Uttar Pradesh")).click();
         $("#city").click();
         $("#city").$(byText("Agra")).click();
-        // нажать кнопку Submit
         $("#submit").click();
-        // Проверить корректность данных заполенных форм
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         $(".modal-body").shouldHave(text("Mikhail Ustinov"));
         $(".modal-body").shouldHave(text("dsvsdv@mail.com"));
